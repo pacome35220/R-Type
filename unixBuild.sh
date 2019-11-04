9efe13ca95ba0535b53ed016b1298ca8b0451968
Pacôme Françon <pacome.francon@epitech.eu>
2019-11-04 11:46:08 +0100

    chore(build): fix all CMakeList

--- unixBuild.sh
+++ unixBuild.sh
@@ -5,5 +5,5 @@ rm -rf ./bin
 mkdir build && cd build && conan install .. --build=missing && cmake .. -G "Unix Makefiles" && cmake --build .
 cd ..
 mkdir bin
-mv build/Dev/Client/bin/r-type_client bin/
-mv build/Dev/Server/bin/r-type_server bin/
+mv build/Client/bin/r-type_client bin/
+mv build/Server/bin/r-type_server bin/
