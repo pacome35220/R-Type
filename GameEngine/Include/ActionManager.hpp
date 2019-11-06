6110c3a60253dd91a2ffd9abeaad552c6b38b611
Camille Police <camillepolice@icloud.com>
2019-11-06 11:31:14 +0100

    feat(GameEngine): ActionManager class

--- GameEngine/Include/ActionManager.hpp
+++ GameEngine/Include/ActionManager.hpp
@@ -5,18 +5,34 @@
 
 #pragma once
 
+#include <map>
+#include <SFML/Window/Keyboard.hpp>
+#include <vector>
+
+using namespace sf;
+
 /**
  * @class ActionManager ActionManager.hpp "Include/ActionManager.hpp"
  */
 class ActionManager
 {
 public:
+    ActionManager();
+    virtual ~ActionManager();
+    void setIsFocused(bool isFocused);
 
 public:
+    bool isKeyPressed(Keyboard::Key key);
+    bool isKeyReleased(Keyboard::Key key);
+    bool isKeyDown(Keyboard::Key key);
+    bool isKeyUp(Keyboard::Key key);
+    bool isKeyMapped(Keyboard::Key key);
+    std::vector<Keyboard::Key> getKeyPressed();
+    void flush();
 
 private:
     bool isFocused;
-    int internalKeyMap[sf::Keyboard::KeyCount];
-    std::map<Key, bool> currentState;
-    std::map<Key, bool> previousState;
+    int internalKeyMap[Keyboard::KeyCount];
+    std::map<Keyboard, bool> currentState;
+    std::map<Keyboard::Key, bool> previousState;
 };
--- GameEngine/Src/ActionManager.cpp
+++ GameEngine/Src/ActionManager.cpp
@@ -10,11 +10,58 @@
 /*
  *   Constructors // Destructor
 */
-
+ActionManager::ActionManager()
+{
+    // TODO
+}
+ActionManager::~ActionManager()
+{
+    // TODO
+}
 /*
  *   Getters // Setters
 */
-
+void ActionManager::setIsFocused(bool isFocused)
+{
+    ActionManager::isFocused = isFocused;
+}
 /*
  *   Methods
-*/ 
+*/
+bool ActionManager::isKeyPressed(Keyboard::Key key)
+{
+    // TODO
+    return false;
+}
+bool ActionManager::isKeyReleased(Keyboard::Key key)
+{
+    // TODO
+    return false;
+}
+bool ActionManager::isKeyDown(Keyboard::Key key)
+{
+    // TODO
+    return false;
+}
+bool ActionManager::isKeyUp(Keyboard::Key key)
+{
+    // TODO
+    return false;
+}
+bool ActionManager::isKeyMapped(Keyboard::Key key)
+{
+    // TODO
+    return false;
+}
+std::vector<Keyboard::Key> ActionManager::getKeyPressed()
+{
+    // TODO
+    return std::vector<Keyboard::Key>();
+}
+/**
+ * Flush all keys
+ */
+void ActionManager::flush()
+{
+    // TODO
+}
