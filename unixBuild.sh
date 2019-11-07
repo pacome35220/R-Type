f5dc6562564457e80bd7aa7c628402cc36065232
Pacôme Françon <pacome.francon@epitech.eu>
2019-11-07 13:06:21 +0100

    chore(): delete useless files

--- unixBuild.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-rm -rf ./build
-rm -rf ./bin
-mkdir build && cd build && conan install .. --build=missing && cmake .. -G "Unix Makefiles" && cmake --build .
-cd ..
-mkdir bin
-mv build/Client/bin/r-type_client bin/
-mv build/Server/bin/r-type_server bin/
