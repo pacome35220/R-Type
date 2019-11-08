ecc9a99423290518a5176e9792a766181d920021
Pacôme Françon <pacome.francon@epitech.eu>
2019-11-08 15:14:20 +0100

    feat(Client): basic client with window

--- Client/Src/Main.cpp
+++ Client/Src/Main.cpp
@@ -1,9 +1,38 @@
+/*
+** EPITECH PROJECT, 2019
+** CPP_rtype_2019
+** File description:
+** main.cpp
+*/
+
 #include <iostream>
+#include <SFML/Graphics.hpp>
+#include "Core/CoreClient.hpp"
+#include "Error.hpp"
+#include "Parallax.hpp"
 
-using namespace std;
+void runClient(const std::string &ip, unsigned short port) {
+    CoreClient core("R-Type Client");
+    // sf::Vector2f pos(0, 0);
+    // sf::Packet packet;
+    // auto parallax = new Parallax(pos);
+    // parallax->setId(-1);
+    // core.addEntity(parallax);
+    std::cout << ip << "mdr" << port << std::endl;
+    core.getNetworkManager()->setIpTarget(ip);
+    core.getNetworkManager()->setPortTarget(port);
+    // core.getAudioManager()->playBgm("./Assets/Audio/BackgroundMusic.ogg");
+    core.run();
+}
 
-int main()
-{
-    cout << "Hello World !" << endl;
-    return 0;
+int main(int argc, char **argv) {
+    if (argc < 3)
+        return EXIT_FAILURE;
+    try {
+        runClient(argv[1], std::stoi(argv[2]));
+    } catch (Error error) {
+        std::cerr << error.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
 }
