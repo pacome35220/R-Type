c6c5403f55546eaabd75ca4a7eb3d479d60153e8
hadi bereksi <hadi-ilies.bereksi-reguig@epitech.eu>
2019-11-18 18:47:20 +0100

    build(GameEngine): CmakeList

--- Entity/Entity.cmake
+++ Entity/Entity.cmake
@@ -1,15 +1,9 @@
-set(ENTITY_SRC Src/)
-set(ENTITY_INC Include/)
+set(ENTITY_INC ${ENTITY_DIR}Include/)
 
 # Includes common directories
 include_directories(${ENTITY_DIR}Include/)
 
-# Append common source file to SRC
-list(APPEND SRC
-
-        )
-
 # Append common include file to INCLUDE
 list(APPEND INCLUDE
         ${ENTITY_INC}EntityList.hpp
-        )
+)
