7852de40976463d584011cf3fd1d93577d6af9f4
Camille Police <camillepolice@icloud.com>
2019-11-19 14:26:01 +0100

    fix(Project): Fix include folders

--- Entity/Entity.cmake
+++ Entity/Entity.cmake
@@ -1,6 +1,7 @@
 set(ENTITY_SRC Entity/Src/)
 set(ENTITY_INC Entity/Include/)
 # Includes common directories
+
 include_directories(${ENTITY_INC})
 include_directories(${ENTITY_INC}/Monster/)
 include_directories(${ENTITY_INC}/Bullet/)
@@ -9,3 +10,11 @@ include_directories(${ENTITY_INC}/Bullet/)
 list(APPEND SRC
         ${ENTITY_SRC}Player.cpp
 )
+
+include_directories(Entity/Include/)
+include_directories()
+# Append common include file to INCLUDE
+list(APPEND SRC
+        Entity/Src/Player.cpp
+)
+
