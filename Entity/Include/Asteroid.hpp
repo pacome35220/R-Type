de5aba8137c97c9366621988f33df072784f4bc3
Camille Police <camillepolice@icloud.com>
2019-11-20 17:52:19 +0100

    feat(Entity): Asteroid

--- Entity/Include/Asteroid.hpp
+++ Entity/Include/Asteroid.hpp
@@ -6,15 +6,23 @@
 #pragma once
 
 #include <AEntity.hpp>
+#include "ACore.hpp"
 
 /**
  * @class Asteroid "Include/Asteroid.hpp"
  */
 class Asteroid : public AEntity {
 public:
+    Asteroid(ACore &core, sf::Vector2f &position);
+    ~Asteroid() = default;
 
 public:
+    static AEntityPtr createPlayerFromPacket(ACore &core, sf::Packet packet);
+    void onCollision(AEntityPtr entity) override;
+    void update() override;
+    void increaseStreamTimer();
 
 private:
-
+    sf::Vector2f originalPos;
+    sf::Vector2f target;
 };
--- Entity/Src/Asteroid.cpp
+++ Entity/Src/Asteroid.cpp
@@ -5,4 +5,43 @@
 ** Asteroid.cpp
 */
 
+#include <cmath>
 #include "Asteroid.hpp"
+
+Asteroid::Asteroid(ACore &core, sf::Vector2f &position) : AEntity(position, core,
+        (enum EntityID)(EI_ASTEROID + rand() % 8 + 1)) {
+
+}
+
+AEntityPtr Asteroid::createPlayerFromPacket(ACore &core, sf::Packet packet) {
+    //TODO
+}
+
+void Asteroid::onCollision(AEntityPtr entity) {
+    if (entity->getEntityType() == EI_PLAYER_BULLET) {
+        this->health--;
+        entryPoint.addToDeletionQueue(getId());
+    }
+}
+
+void Asteroid::update() {
+    this->position.x -= 1;
+    double deltaY = originalPos.y + target.y;
+    double deltaX = originalPos.x + target.x;
+    this->position.y += (deltaY / deltaX);
+
+    if (this->position.x < -110) {
+        this->position.x = 110;
+        this->position.y = rand() % 50 - 100;
+    }
+    if (this->health < 0) {
+        this->position.x = 110;
+        this->position.y = rand() % 50 - 100;
+        this->entryPoint.feedEntity(std::make_shared<Asteroid>(position, rand() % 7 + 1));
+        this->entryPoint.addToDeletionQueue(this->getId());
+    }
+}
+
+void Asteroid::increaseStreamTimer() {
+    streamTimer += 15;
+}
