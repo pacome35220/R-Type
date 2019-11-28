4eb4308dffc500d6412f5466f616ce6736b9bc87
Pacôme Françon <pacome.francon@epitech.eu>
2019-11-28 02:17:25 +0100

    refacto(Client): reorder feedFactory list + remove 1 EntityID from enum

--- Client/Main.cpp
+++ Client/Main.cpp
@@ -33,11 +33,13 @@
  * @param core
  */
 void feedTextureFactory(std::shared_ptr<Manager::Resource> resource) {
-    resource->feedTextureFactory(EI_PARALLAX, "./Assets/Sprites/starfieldBlue.png");
-    resource->feedTextureFactory(EI_PLAYER1, "./Assets/Sprites/Player1.png");
-    resource->feedTextureFactory(EI_PLAYER2, "./Assets/Sprites/Player2.png");
-    resource->feedTextureFactory(EI_PLAYER3, "./Assets/Sprites/Player3.png");
-    resource->feedTextureFactory(EI_PLAYER4, "./Assets/Sprites/Player4.png");
+    resource->feedTextureFactory(EI_CLASSIC_MONSTER, "./Assets/Sprites/Monster/Classic.png");
+    resource->feedTextureFactory(EI_DUMB_MONSTER, "./Assets/Sprites/Monster/Dumb.png");
+    resource->feedTextureFactory(EI_SPEED_MONSTER, "./Assets/Sprites/Monster/Speed.png");
+    resource->feedTextureFactory(EI_ANGRY_MONSTER, "./Assets/Sprites/Monster/Angry.png");
+    resource->feedTextureFactory(EI_DOLLEY_MONSTER, "./Assets/Sprites/Monster/Dolley.png");
+
+    resource->feedTextureFactory(EI_DEATH_STAR, "./Assets/Sprites/BlackStar.png");
 
     resource->feedTextureFactory(EI_ASTEROID1, "./Assets/Sprites/Asteroid1.png");
     resource->feedTextureFactory(EI_ASTEROID2, "./Assets/Sprites/Asteroid2.png");
@@ -46,24 +48,20 @@ void feedTextureFactory(std::shared_ptr<Manager::Resource> resource) {
     resource->feedTextureFactory(EI_ASTEROID5, "./Assets/Sprites/Asteroid5.png");
     resource->feedTextureFactory(EI_ASTEROID6, "./Assets/Sprites/Asteroid6.png");
     resource->feedTextureFactory(EI_ASTEROID7, "./Assets/Sprites/Asteroid7.png");
-    resource->feedTextureFactory(EI_ASTEROID8, "./Assets/Sprites/Asteroid8.png");
 
-    resource->feedTextureFactory(EI_POWER_UP, "./Assets/Sprites/PowerUp.png");
-
-    resource->feedTextureFactory(EI_PLAYER_BULLET, "./Assets/Sprites/PlayerBullet.png");
     resource->feedTextureFactory(EI_DEATH_STAR_BULLET, "./Assets/Sprites/BlackStarBullet.png");
     resource->feedTextureFactory(EI_MONSTER_BULLET, "./Assets/Sprites/MonsterBullet.png");
+    resource->feedTextureFactory(EI_PLAYER_BULLET, "./Assets/Sprites/PlayerBullet.png");
 
-    resource->feedTextureFactory(EI_DEATH_STAR, "./Assets/Sprites/BlackStar.png");
-    resource->feedTextureFactory(EI_ANGRY_MONSTER, "./Assets/Sprites/Monster/Angry.png");
-    resource->feedTextureFactory(EI_CLASSIC_MONSTER, "./Assets/Sprites/Monster/Classic.png");
-    resource->feedTextureFactory(EI_DOLLEY_MONSTER, "./Assets/Sprites/Monster/Dolley.png");
-    resource->feedTextureFactory(EI_DUMB_MONSTER, "./Assets/Sprites/Monster/Dumb.png");
-    resource->feedTextureFactory(EI_SPEED_MONSTER, "./Assets/Sprites/Monster/Speed.png");
+    resource->feedTextureFactory(EI_PARALLAX, "./Assets/Sprites/starfieldBlue.png");
 
-    resource->feedTextureFactory(EI_HEALTH_UP, "./Assets/Sprites/Monster/HealthUp.png");
+    resource->feedTextureFactory(EI_POWER_UP, "./Assets/Sprites/PowerUp.png");
+    resource->feedTextureFactory(EI_HEALTH_UP, "./Assets/Sprites/HealthUp.png");
 
-    resource->feedTextureFactory(EI_SPEED_MONSTER, "./Assets/Sprites/Monster/Speed.png");
+    resource->feedTextureFactory(EI_PLAYER1, "./Assets/Sprites/Player1.png");
+    resource->feedTextureFactory(EI_PLAYER2, "./Assets/Sprites/Player2.png");
+    resource->feedTextureFactory(EI_PLAYER3, "./Assets/Sprites/Player3.png");
+    resource->feedTextureFactory(EI_PLAYER4, "./Assets/Sprites/Player4.png");
 }
 
 /**
@@ -80,9 +78,8 @@ void feedEntityFactory(std::shared_ptr<Manager::Network> network) {
     network->getEntityFactory().addEntity(EI_ASTEROID5, Asteroid::createAsteroidFromPacket);
     network->getEntityFactory().addEntity(EI_ASTEROID6, Asteroid::createAsteroidFromPacket);
     network->getEntityFactory().addEntity(EI_ASTEROID7, Asteroid::createAsteroidFromPacket);
-    network->getEntityFactory().addEntity(EI_ASTEROID8, Asteroid::createAsteroidFromPacket);
 
-    // network->getEntityFactory().addEntity(EI_DEATH_STAR, DeathStar::createDeathStarFromPacket);
+    network->getEntityFactory().addEntity(EI_DEATH_STAR, DeathStar::createDeathStarFromPacket);
 
     network->getEntityFactory().addEntity(EI_PLAYER1, Player::createPlayerFromPacket);
     network->getEntityFactory().addEntity(EI_PLAYER2, Player::createPlayerFromPacket);
