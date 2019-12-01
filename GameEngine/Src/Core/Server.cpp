a35862f8502d7b96c5703bfb73d1110001945e05
Pacôme Françon <pacome.francon@epitech.eu>
2019-12-01 17:46:35 +0100

    refacto(GameEngine): remove useless comments

--- GameEngine/Src/Core/Server.cpp
+++ GameEngine/Src/Core/Server.cpp
@@ -51,7 +51,6 @@ void Core::Server::updateEntities() {
 
 void Core::Server::procDeletionQueue() {
     for (std::size_t i = 0 ; i < this->deletionQueue.size() ; i++) {
-        // for (const auto &entityToDelete : this->deletionQueue) {
         auto tmp = std::find(this->entities.begin(), this->entities.end(),
                              this->deletionQueue[i]);
         if (tmp != this->entities.end()) {
