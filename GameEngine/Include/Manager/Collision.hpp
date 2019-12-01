7afa73878c3c94dfa5ad5b43bd2ccaff83c603d4
Camille Police <camillepolice@icloud.com>
2019-12-01 11:33:17 +0100

    refacto(GameEngine/Manager): Collision

--- GameEngine/Include/Manager/Collision.hpp
+++ GameEngine/Include/Manager/Collision.hpp
@@ -14,21 +14,22 @@
 /**
  * @namespace Package to manage whole entities in the project
  */
-namespace Manager
-{
-/**
- * @class Collision "Include/Collision.hpp"
- */
-    class Collision
-    {
+namespace Manager {
+    /**
+     * @class Collision "Include/Collision.hpp"
+    */
+    class Collision {
     public:
         Collision() = default;
         ~Collision() = default;
 
-        void detectCollision(const std::vector<AEntityPtr> &entityList);
+    public:
+        void detectCollision(const std::vector <AEntityPtr> &entityList);
 
     private:
-        void processCollision(std::list<AEntityPtr> &entityList1, std::list<AEntityPtr> &entityList2);
-        bool entityCollision(const AEntityPtr &entity1, const AEntityPtr &entity2);
+        void processCollision(std::list <AEntityPtr> &entityList1,
+                              std::list <AEntityPtr> &entityList2);
+        bool entityCollision(const AEntityPtr &entity1, const AEntityPtr
+        &entity2);
     };
 }
--- GameEngine/Src/Manager/Collision.cpp
+++ GameEngine/Src/Manager/Collision.cpp
@@ -2,30 +2,38 @@
 ** EPITECH PROJECT, 2019
 ** CPP_rtype_2019
 ** File description:
-** Collision.cpp
+** Manager/Collision.cpp
 */
 
 #include <iostream>
 #include "Manager/Collision.hpp"
 
+void Manager::Collision::detectCollision(
+        const std::vector <AEntityPtr> &entityList) {
+    std::list <AEntityPtr> collider;
 
-bool Manager::Collision::entityCollision(const AEntityPtr &entity1, const AEntityPtr &entity2)
-{
-    sf::FloatRect colliderRect = entity1->getSprite().getGlobalBounds();
-    sf::FloatRect otherRect = entity2->getSprite().getGlobalBounds();
+    if (entityList.size() < 3)
+        return;
+    for (auto &entity: entityList)
+        if (entity->getEntityType() != EI_PARALLAX)
+            collider.push_back(entity);
+    std::cout << "Collision::detectCollision between " << collider.size()
+              << " entities" << std::endl;
+    this->processCollision(collider, collider);
 
-    return colliderRect.intersects(otherRect);
 }
 
-void Manager::Collision::processCollision(std::list<AEntityPtr> &entityList1, std::list<AEntityPtr> &entityList2)
-{
+void Manager::Collision::processCollision(std::list <AEntityPtr> &entityList1,
+                                          std::list <AEntityPtr> &entityList2) {
     for (auto const &current: entityList1) {
         for (auto const &other: entityList2) {
-            if (current->getId() == other->getId() ||
-                current->getEntityType() == other->getEntityType())
+            if (current->getId() == other->getId()
+                || current->getEntityType() == other->getEntityType())
                 continue;
-            if (this->entityCollision(current, other) || this->entityCollision(other, current)) {
-                std::cout << "Collision match : " << current->getEntityType() << " - " << other->getEntityType() << std::endl;
+            if (this->entityCollision(current, other)
+                || this->entityCollision(other, current)) {
+                std::cout << "Collision match : " << current->getEntityType()
+                          << " - " << other->getEntityType() << std::endl;
                 current->onCollision(other);
                 other->onCollision(current);
             }
@@ -33,16 +41,10 @@ void Manager::Collision::processCollision(std::list<AEntityPtr> &entityList1, st
     }
 }
 
-void Manager::Collision::detectCollision(const std::vector<AEntityPtr> &entityList)
-{
-    std::list<AEntityPtr> collider;
+bool Manager::Collision::entityCollision(const AEntityPtr &entity1,
+                                         const AEntityPtr &entity2) {
+    sf::FloatRect colliderRect = entity1->getSprite().getGlobalBounds();
+    sf::FloatRect otherRect = entity2->getSprite().getGlobalBounds();
 
-    if (entityList.size() < 3)
-        return;
-    for (auto &entity: entityList)
-        if (entity->getEntityType() != EI_PARALLAX)
-            collider.push_back(entity);
-    std::cout << "Collision::detectCollision between " << collider.size() << " entities" << std::endl;
-    this->processCollision(collider, collider);
-
+    return colliderRect.intersects(otherRect);
 }
